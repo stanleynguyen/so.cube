0e4f47662fb6dc74be8ed6d219c1ab0fdbefa288
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-03-26 21:57:53 +0800

    Experiment color animation

--- script.js
+++ script.js
@@ -12,12 +12,27 @@ function draw() {
     ['France', 600],
     ['RU', 700],
   ]);
+  const alterData = GoogleCharts.api.visualization.arrayToDataTable([
+    ['Country', 'Popularity'],
+    ['Germany', 200],
+    ['United States', 3000],
+    ['Brazil', 400],
+    ['Canada', 500],
+    ['France', 600],
+    ['RU', 7],
+  ]);
 
-  var options = {};
+  const options = {
+    animation: {
+      duration: 10000,
+      easing: 'out',
+    },
+  };
 
   var chart = new GoogleCharts.api.visualization.GeoChart(
     document.getElementById('root'),
   );
 
   chart.draw(data, options);
+  setTimeout(chart.draw.bind(chart, alterData, options), 5000);
 }
